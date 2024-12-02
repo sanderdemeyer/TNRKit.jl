6c32d1a5c87e9e4d4988e5b2b726f8da77659975
victor <victor.vanthilt@gmail.com>
2024-12-02 12:15:53 +0100

    rename truncationscheme in example.jl

--- test/example.jl
+++ test/example.jl
@@ -9,23 +9,23 @@ trg_f(steps::Int, data) = abs(log(data[end]) * 2.0^(-steps))
 stopping_criterion = convcrit(1e-16, trg_f)&maxiter(20)
 
 # choose a TensorKit truncation scheme
-truncate = truncdim(16)&truncbelow(1e-40)
+trunc = truncdim(16)&truncbelow(1e-40)
 
 # initialize the TRG scheme
 scheme = TRG(classical_ising(1.0))
 
 # run the TRG scheme (and normalize and store the norm in the beginning (finalize_beginning=true))
-data = run!(scheme, truncate, stopping_criterion; finalize_beginning=true)
+data = run!(scheme, trunc, stopping_criterion; finalize_beginning=true)
 # or: data = run!(scheme, truncdim(16)), this will default to maxiter(100)
 
 # initialize the BTRG scheme
 scheme = BTRG(classical_ising(1.0), -0.5)
 
 # run the BTRG scheme
-data = run!(scheme, truncate, stopping_criterion)
+data = run!(scheme, trunc, stopping_criterion)
 
 # initialize the HOTRG scheme
 scheme = HOTRG(classical_ising(1.0))
 
 # run the HOTRG scheme
-data = run!(scheme, truncate, stopping_criterion)
+data = run!(scheme, trunc, stopping_criterion)
