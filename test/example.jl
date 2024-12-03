c9538fbbe0f6e660927673e1a8c8b94dc0b52e5b
Adwait-Naravane <adwaitnaravane@gmail.com>
2024-12-03 17:32:55 +0100

    atsushi

--- test/example.jl
+++ test/example.jl
@@ -12,7 +12,7 @@ stopping_criterion = convcrit(1e-20, trg_f)&maxiter(200)
 scheme = TRG(triangle_bad())
 
 # run the TRG scheme (and normalize and store the norm in the beginning (finalize_beginning=true))
-data = run!(scheme, truncdim(64), stopping_criterion; finalize_beginning=false)
+data = run!(scheme, truncdim(16), stopping_criterion; finalize_beginning=false)
 # or: data = run!(scheme, truncdim(16)), this will default to maxiter(100)
 lnz_triangle_bad = 0
 for (i, d) in enumerate(data)
@@ -21,12 +21,12 @@ end
 @show lnz_triangle_bad
 
 # initialize the BTRG scheme
-scheme = BTRG(triangle_bad(1.0), -0.5)
+scheme = BTRG(triangle_bad(), 1)
 
 # run the BTRG scheme
-data = run!(scheme, truncdim(16), stopping_criterion; finalize_beginning=true)
+data = run!(scheme, truncdim(16), stopping_criterion; finalize_beginning=false)
 lnz_triangle_bad = 0
 for (i, d) in enumerate(data)
-    lnz_triangle_bad += log(d) * 4.0^(1-i)        
+    lnz_triangle_bad += log(d) * 2.0^(-i)        
 end
 @show lnz_triangle_bad
