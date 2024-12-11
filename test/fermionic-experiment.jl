3d9f1160ea99b5d0b23bdb26fd3b984f922c1232
Adwait-Naravane <adwaitnaravane@gmail.com>
2024-12-11 12:40:24 +0100

    removed old convergence

--- test/fermionic-experiment.jl
+++ test/fermionic-experiment.jl
@@ -23,10 +23,10 @@ using .TRGKit
 # end
 
 custom_convcrit(steps::Int, data) = abs(log(data[end]) * 2.0^(1-steps))
-custom_convcrit_hot(steps::Int, data) = abs(log(data[end]) * 4.0^(1-steps))
+custom_convcrit_hot(steps::Int, data) = abs(log(data[end]) * 2.0^(1-steps))
 
 ms = [0]
-χs = [20]
+χs = [8]
 
 for m in ms
     lnz_trgs = []
@@ -35,30 +35,30 @@ for m in ms
     for χ in χs
         T = gross_neveu_start(0, m, 0)
         # trg = TRG(copy(T); finalize=fermionic_trg_finalize!)
-        btrg = BTRG(copy(T))
-        #hotrg = HOTRG(copy(T);  finalize=fermionic_hotrg_finalize!)
+        #btrg = BTRG(copy(T))
+        hotrg = HOTRG(copy(T))
         # data_trg = run!(trg, truncdim(χ), convcrit(1e-20, custom_convcrit))
-        data_btrg = run!(btrg, truncdim(χ), convcrit(1e-20, custom_convcrit))
-        #data_hotrg = run!(hotrg, truncdim(χ), convcrit(1e-20, custom_convcrit_hot))
+        #data_btrg = run!(btrg, truncdim(χ), convcrit(1e-20, custom_convcrit))
+        data_hotrg = run!(hotrg, truncdim(χ), convcrit(1e-20, custom_convcrit_hot))
 
         # lnz_trg = 0
         # for (i, d) in enumerate(data_trg)
         #     lnz_trg += log(d) * 2.0^(1-i)
         # end
 
-        lnz_btrg = 0
-        for (i, d) in enumerate(data_btrg)
-            lnz_btrg += log(d) * 2.0^(1-i)
-        end
-        @show lnz_btrg
-        # lnz_hotrg = 0
-        # for (i, d) in enumerate(data_hotrg)
-        #     lnz_hotrg += log(d) * 4.0^(1-i)
+        # lnz_btrg = 0
+        # for (i, d) in enumerate(data_btrg)
+        #     lnz_btrg += log(d) * 2.0^(1-i)
         # end
-        # @show lnz_trg
+        # @show lnz_btrg
+        lnz_hotrg = 0
+        for (i, d) in enumerate(data_hotrg)
+            lnz_hotrg += log(d) * 2.0^(1-i)
+        end
+        @show lnz_hotrg
 
         # push!(lnz_trgs, lnz_trg)
-        push!(lnz_btrgs, lnz_btrg)
+        #push!(lnz_btrgs, lnz_btrg)
         #push!(lnz_hotrgs, lnz_hotrg)
     end
     #CSV.write("data/fermionic_m$(m).csv", DataFrame(χ=χs, lnz_trg=lnz_trgs, lnz_btrg=lnz_btrgs, lnz_hotrg=lnz_hotrgs))
