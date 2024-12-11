7d04afc6353223160db0564043d103d2022b7245
Adwait-Naravane <adwaitnaravane@gmail.com>
2024-12-11 16:31:49 +0100

    Hubbard is hard

--- test/fermionic-experiment.jl
+++ test/fermionic-experiment.jl
@@ -26,38 +26,39 @@ custom_convcrit(steps::Int, data) = abs(log(data[end]) * 2.0^(1-steps))
 custom_convcrit_hot(steps::Int, data) = abs(log(data[end]) * 2.0^(1-steps))
 
 ms = [0]
-χs = [8]
+χs = [16]
 
 for m in ms
     lnz_trgs = []
     lnz_btrgs = []
     lnz_hotrgs = []
     for χ in χs
-        T = gross_neveu_start(0, m, 0)
-        # trg = TRG(copy(T); finalize=fermionic_trg_finalize!)
-        #btrg = BTRG(copy(T))
-        hotrg = HOTRG(copy(T))
-        # data_trg = run!(trg, truncdim(χ), convcrit(1e-20, custom_convcrit))
-        #data_btrg = run!(btrg, truncdim(χ), convcrit(1e-20, custom_convcrit))
-        data_hotrg = run!(hotrg, truncdim(χ), convcrit(1e-20, custom_convcrit_hot))
+        T = Hubbard2D_start(2, 1, 0.0001, 4)
+        #trg = TRG(copy(T))
+        btrg = BTRG(copy(T))
+        #hotrg = HOTRG(copy(T))
+        #data_trg = run!(trg, truncdim(χ), convcrit(1e-20, custom_convcrit))
+        data_btrg = run!(btrg, truncdim(χ), convcrit(1e-20, custom_convcrit))
+        #data_hotrg = run!(hotrg, truncdim(χ), convcrit(1e-20, custom_convcrit_hot))
 
         # lnz_trg = 0
         # for (i, d) in enumerate(data_trg)
         #     lnz_trg += log(d) * 2.0^(1-i)
         # end
+        # @show lnz_trg
 
-        # lnz_btrg = 0
-        # for (i, d) in enumerate(data_btrg)
-        #     lnz_btrg += log(d) * 2.0^(1-i)
-        # end
-        # @show lnz_btrg
-        lnz_hotrg = 0
-        for (i, d) in enumerate(data_hotrg)
-            lnz_hotrg += log(d) * 2.0^(1-i)
+        lnz_btrg = 0
+        for (i, d) in enumerate(data_btrg)
+            lnz_btrg += log(d) * 2.0^(1-i)
         end
-        @show lnz_hotrg
+        @show lnz_btrg
+        # lnz_hotrg = 0
+        # for (i, d) in enumerate(data_hotrg)
+        #     lnz_hotrg += log(d) * 2.0^(1-i)
+        # end
+        # @show lnz_hotrg
 
-        # push!(lnz_trgs, lnz_trg)
+        #push!(lnz_trgs, lnz_trg)
         #push!(lnz_btrgs, lnz_btrg)
         #push!(lnz_hotrgs, lnz_hotrg)
     end
