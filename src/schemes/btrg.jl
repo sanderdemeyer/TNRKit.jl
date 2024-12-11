342f8ae31170d2b27f5d403fd78130b9797c15c4
victor <victor.vanthilt@gmail.com>
2024-12-11 10:41:32 +0100

    change BTRG to match the scripture

--- src/schemes/btrg.jl
+++ src/schemes/btrg.jl
@@ -8,7 +8,7 @@ mutable struct BTRG <: TRGScheme
     finalize!::Function
     function BTRG(T::TensorMap, k::Number; finalize=finalize!)
         # Construct S1 and S2 as identity matrices.
-        new(T, id(space(T, 1)), id(space(T, 1)), k, finalize)
+        new(T, id(space(T, 2)), id(space(T, 1)), k, finalize)
     end
 end
 
@@ -25,50 +25,45 @@ function pseudopow(t::TensorMap, a::Real; tol=eps(scalartype(t))^(3 / 4))
     return t′
 end
 
-
 function step!(scheme::BTRG, trunc::TensorKit.TruncationScheme)
-    U, S, V, _ϵ = tsvd(scheme.T, (1, 4), (2, 3); trunc=trunc)
+    U, S, V, ε = tsvd(scheme.T, (1, 2), (3, 4); trunc=trunc)
 
-    # make the legs normal again
+    S_a = pseudopow(S, (1 - scheme.k) / 2)
+    S_b = pseudopow(S, scheme.k)
+
+    @plansor begin
+        A[-1 -2; -3] := U[-1 -2; 1] * S_a[1; -3]
+        B[-1; -2 -3] := S_a[-1; 1] * V[1; -2 -3]
+        S1′[-1; -2] := S_b[-1; -2]
+    end
+
+    U, S, V, ε = tsvd(scheme.T, (1, 4), (2, 3); trunc=trunc)
+    
+    # permute to correct the spaces
     U = permute(U, (1,), (2, 3))
     V = permute(V, (1, 2), (3,))
 
     S_a = pseudopow(S, (1 - scheme.k) / 2)
     S_b = pseudopow(S, scheme.k)
-
+    
     @plansor begin
-        A[-1; -2 -3] := U[-1; -2 1] * S_a[1; -3]
-        B[-1 -2; -3] := S_a[-1; 1] * V[1 -2; -3]
-        E[-1; -2] := S_b[-1; -2]
-    end
-
-    U, S, V, _ϵ = tsvd(scheme.T, (1, 2), (3, 4); trunc=trunc)
-
-    # spaces are already correct
-    S_a = pseudopow(S, (1 - scheme.k) / 2)
-    S_b = pseudopow(S, scheme.k)
-
-    @plansor begin
-        C[-1 -2; -3] := U[-1 -2; 1] * S_a[1; -3]
-        D[-1; -2 -3] := S_a[-1; 1] * V[1; -2 -3]
-        F[-1; -2] := S_b[-1; -2]
+        C[-1; -2 -3] := U[-1; -2 1] * S_a[1; -3]
+        D[-1 -2; -3] := S_a[-1; 1] * V[1 -2; -3]
+        S2′[-1; -2] := S_b[-1; -2]
     end
 
     S1 = scheme.S1
     S2 = scheme.S2
 
-    @tensor T′[-1 -2; -3 -4] := B[-1 1; 8] * S1[2; 1] * D[-2; 3 2] * S2[3; 4] * A[4; 5 -3] * S1[5; 6] * C[7 6; -4] * S2[8; 7]
+    @tensor T′[-1 -2; -3 -4] := D[-1 1; 8] * S1[2; 1] * B[-2; 3 2] * S2[3; 4] * C[4; 5 -3] * S1[5; 6] * A[7 6; -4] * S2[8; 7]
 
     scheme.T = T′
-    scheme.S1 = E
-    scheme.S2 = F
+    scheme.S1 = S1′
+    scheme.S2 = S2′
     return scheme
 end
 
 function finalize!(scheme::BTRG)
-    # Turn and mirror
-    scheme.T = permute(scheme.T, (2, 1), (4, 3))
-    # calculate norm
     n = norm(@tensor scheme.T[1 2; 3 4] * scheme.S1[4; 2] * scheme.S2[3; 1])
     scheme.T /= n
     return n
