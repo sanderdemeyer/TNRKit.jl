75f286c10498fa14dcae6657981daa5d31e89357
Adwait-Naravane <adwaitnaravane@gmail.com>
2024-12-17 22:30:40 +0100

    Implementing Boundary TRG

---
+++ src/schemes/boundary_trg.jl
@@ -0,0 +1,57 @@
+mutable struct Boundary_TRG <: TRGScheme
+    T::TensorMap
+    E1::TensorMap
+    E2::TensorMap
+    finalize!::Function
+    function Boundary_TRG(T::TensorMap, E1::TensorMap, E2::TensorMap; finalize=finalize!)
+        new(T, E1, E2, finalize)
+    end
+end
+
+
+function step!(scheme::Boundary_TRG, trunc::TensorKit.TruncationScheme)
+    # The boundary tensors are all along the horizontal direction
+    # Contract along the vertical direction
+    @tensor temp_bound_north[-1 -2 -3; -4] := scheme.E1[-1 -2; 1]*scheme.E1[1 -3; -4]
+    @tensor temp_bulk[-1 -2 -3; -4 -5 -6] = scheme.T[-1 -2; 1 -6]*scheme.T[1 -3; -4 -5]
+
+    #construct projectors for vertical contraction
+    R1, _ = rightorth(temp_bound_north, (2,3), (4,1))
+    _, R2 = leftorth(temp_bulk, (1,2,3,4), (5,6))
+    R3, _ = rightorth(temp_bulk, (2,3), (4,5,6,1))
+
+    @tensor R1R2[-1; -2] := R2[-1; 1 2] * R1[2 1; -2]
+    @tensor R3R2[-1; -2] := R2[-1; 1 2] * R3[2 1; -2]
+
+    U, S, V, _ = tsvd(R1R2, (1,), (2,); trunc = trunc)
+    inv_s = pseudopow(S, -0.5)
+    #TODO: Fix the domains below
+    @tensor Proj_1[-1 -2; -3] := R2[1; -2 -3] * adjoint(U)[2; 1] * inv_s[-1; 2]
+    @tensor Proj_2[-1 -2;-3] := inv_s[1; -3] * adjoint(V)[2; 1] * R1[-1 -2 ;2]
+
+    U, S, V, _ = tsvd(R3R2, (1,), (2,); trunc = trunc)
+    inv_s = pseudopow(S, -0.5)
+    @tensor Proj_3[-1 -2; -3] := R2[1; -2 -3] * adjoint(U)[2; 1] * inv_s[-1; 2]
+    @tensor Proj_4[-1 -2;-3] := inv_s[1; -3] * adjoint(V)[2; 1] * R3[-1 -2 ;2]
+
+    #renormalise the tensors along vertical
+    @tensor temp_E1[-1 -2; -3] := temp_bound_north[-1 1 2; -3]*Proj_1[2 1; -2]
+    @tensor bulk_env[-1 -2; -3 -4] := temp_bulk[-1 1 2; -3 3 4]*Proj_3[2 1; -2]*Proj_2[4 3; -4]
+    @tensor bulk_temp[-1 -2; -3 -4] := temp_bulk[-1 1 2; -3 3 4]*Proj_3[2 1; -2]*Proj_2[4 3; -4]
+    
+    return scheme
+end
+#TODO: The below function is much more complicated than the one for the infinite lattice models.
+function finalize!(scheme::Boundary_TRG)
+    n = norm(@tensor scheme.T[1 2; 1 2])
+    scheme.T /= n
+
+    # turn the tensor by 90 degrees
+    scheme.T = permute(scheme.T, ((2, 3), (4, 1)))
+
+    return n
+end
+
+# example convcrit function
+hotrg_robust_convcrit(steps::Int, data) = abs(log(data[end]) * 2.0^(-steps))
+
