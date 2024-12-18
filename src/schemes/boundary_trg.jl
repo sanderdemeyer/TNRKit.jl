b1042546591901698b19cea67d998cdc0c0b0ecf
Adwait-Naravane <adwaitnaravane@gmail.com>
2024-12-18 14:22:08 +0100

    Added the rough scheme of Boundary-TRG

--- src/schemes/boundary_trg.jl
+++ src/schemes/boundary_trg.jl
@@ -25,20 +25,50 @@ function step!(scheme::Boundary_TRG, trunc::TensorKit.TruncationScheme)
 
     U, S, V, _ = tsvd(R1R2, (1,), (2,); trunc = trunc)
     inv_s = pseudopow(S, -0.5)
-    #TODO: Fix the domains below
-    @tensor Proj_1[-1 -2; -3] := R2[1; -2 -3] * adjoint(U)[2; 1] * inv_s[-1; 2]
+
+    
+    @tensor Proj_1[-1;-2 -3] := R2[1; -2 -3] * adjoint(U)[2; 1] * inv_s[-1; 2]
     @tensor Proj_2[-1 -2;-3] := inv_s[1; -3] * adjoint(V)[2; 1] * R1[-1 -2 ;2]
 
     U, S, V, _ = tsvd(R3R2, (1,), (2,); trunc = trunc)
     inv_s = pseudopow(S, -0.5)
-    @tensor Proj_3[-1 -2; -3] := R2[1; -2 -3] * adjoint(U)[2; 1] * inv_s[-1; 2]
+    @tensor Proj_3[-1;-2 -3] := R2[1; -2 -3] * adjoint(U)[2; 1] * inv_s[-1; 2]
     @tensor Proj_4[-1 -2;-3] := inv_s[1; -3] * adjoint(V)[2; 1] * R3[-1 -2 ;2]
 
     #renormalise the tensors along vertical
-    @tensor temp_E1[-1 -2; -3] := temp_bound_north[-1 1 2; -3]*Proj_1[2 1; -2]
-    @tensor bulk_env[-1 -2; -3 -4] := temp_bulk[-1 1 2; -3 3 4]*Proj_3[2 1; -2]*Proj_2[4 3; -4]
-    @tensor bulk_temp[-1 -2; -3 -4] := temp_bulk[-1 1 2; -3 3 4]*Proj_3[2 1; -2]*Proj_2[4 3; -4]
+    @tensor temp_E1[-1 -2; -3] := temp_bound_north[-1 1 2; -3]*Proj_1[-2; 2 1]
+    @tensor bulk_env[-1 -2; -3 -4] := temp_bulk[-1 1 2; -3 3 4]*Proj_3[-2; 2 1]*Proj_2[4 3; -4]
+    @tensor bulk_temp[-1 -2; -3 -4] := temp_bulk[-1 1 2; -3 3 4]*Proj_3[-2; 2 1]*Proj_4[4 3; -4]
     
+    #contract along the horizontal direction
+
+    @tensor another_north[-1 -2 -3; -4 -5] := temp_E1[-1 1; -5]* bulk_env[-2 -3; -4 1]
+    @tensor another_bulk[-1 -2 -3; -4 -5 -6] := bulk_temp[-1 1; -5 -6]*bulk_temp[-2 -3; -4 1]
+
+    #construct projectors for horizontal contraction
+    R1, _ = rightorth(another_north, (1,2), (3,4,5))
+    _, R2 = leftorth(another_north, (1,2,3), (4,5))
+    R3, _ = rightorth(another_bulk, (1,2), (3,4,5,6))
+    _, R4 = leftorth(another_bulk, (1,2,3,6), (4,5))
+
+    @tensor R2R1[-1; -2] := R2[-1; 1 2] * R1[2 1; -2]
+    @tensor R4R3[-1; -2] := R4[-1; 1 2] * R3[2 1; -2]
+
+    U, S, V, _ = tsvd(R2R1, (1,), (2,); trunc = trunc)
+    inv_s = pseudopow(S, -0.5)
+    @tensor Proj_5[-1 -2; -3] := R1[-1 -2; 1] * adjoint(V)[1; 2] * inv_s[2; -3]
+    @tensor Proj_6[-1; -2 -3] := inv_s[-1; 1] * adjoint(U)[1; 2] * R2[2; -2 -3]
+
+    U, S, V, _ = tsvd(R4R3, (1,), (2,); trunc = trunc)
+    inv_s = pseudopow(S, -0.5)
+    @tensor Proj_7[-1 -2; -3] := R3[-1 -2; 1] * adjoint(V)[1; 2] * inv_s[2; -3]
+    @tensor Proj_8[-1; -2 -3] := inv_s[-1; 1] * adjoint(U)[1; 2] * R4[2; -2 -3]
+
+    #renormalise the tensors along horizontal
+    @tensor scheme.E1[-1 -2; -3] := Proj_6[-1; 2 1]*another_north[1 2 -2; 3 4]*Proj_5[4 3; -3]
+    @tensor scheme.T[-1 -2; -3 -4] := Proj_8[-1; 2 1]*another_bulk[1 2 -2; 3 4 -4]*Proj_7[4 3; -3]
+    
+
     return scheme
 end
 #TODO: The below function is much more complicated than the one for the infinite lattice models.
