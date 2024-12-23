60b9c32451c13abb903942d70edb1e962e982b5b
Adwait Naravane <anaravan@erquaf.UGent.be>
2024-12-23 14:04:14 +0100

    merging

--- test/fermionic-experiment.jl
+++ test/fermionic-experiment.jl
@@ -33,31 +33,32 @@ for m in ms
     lnz_btrgs = []
     lnz_hotrgs = []
     for χ in χs
-        T = gross_neveu_start(0, m, 0)
-        # trg = TRG(copy(T); finalize=fermionic_trg_finalize!)
+        T = Hubbard2D_start(0, 1, 0.0001, 0)
+        trg = TRG(copy(T); finalize=fermionic_trg_finalize!)
         #btrg = BTRG(copy(T))
-        hotrg = HOTRG(copy(T))
-        # data_trg = run!(trg, truncdim(χ), convcrit(1e-20, custom_convcrit))
+        #hotrg = HOTRG(copy(T))
+        data_trg = run!(trg, truncdim(χ), convcrit(1e-20, custom_convcrit))
         #data_btrg = run!(btrg, truncdim(χ), convcrit(1e-20, custom_convcrit))
-        data_hotrg = run!(hotrg, truncdim(χ), convcrit(1e-20, custom_convcrit_hot))
+        #data_hotrg = run!(hotrg, truncdim(χ), convcrit(1e-20, custom_convcrit_hot))
 
-        # lnz_trg = 0
-        # for (i, d) in enumerate(data_trg)
-        #     lnz_trg += log(d) * 2.0^(1-i)
-        # end
+        lnz_trg = 0
+        for (i, d) in enumerate(data_trg)
+            lnz_trg += log(d) * 2.0^(1-i)
+        end
+        @show lnz_trg
 
         # lnz_btrg = 0
         # for (i, d) in enumerate(data_btrg)
         #     lnz_btrg += log(d) * 2.0^(1-i)
         # end
         # @show lnz_btrg
-        lnz_hotrg = 0
-        for (i, d) in enumerate(data_hotrg)
-            lnz_hotrg += log(d) * 2.0^(1-i)
-        end
-        @show lnz_hotrg
+        # lnz_hotrg = 0
+        # for (i, d) in enumerate(data_hotrg)
+        #     lnz_hotrg += log(d) * 2.0^(1-i)
+        # end
+        # @show lnz_hotrg
 
-        # push!(lnz_trgs, lnz_trg)
+        #push!(lnz_trgs, lnz_trg)
         #push!(lnz_btrgs, lnz_btrg)
         #push!(lnz_hotrgs, lnz_hotrg)
     end
