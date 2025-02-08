797ddb28031602261ca3ecbe2a742f5058c5975d
Victor Vanthilt <73738005+VictorVanthilt@users.noreply.github.com>
2025-02-08 17:19:12 +0100

    Implement clock model (#13)
    * clock_model
    
    * format and remove notebook junk
    
    ---------
    
    Co-authored-by: dartsushi <aueda.phy@gmial.com>

--- src/models/ising.jl
+++ src/models/ising.jl
@@ -56,3 +56,21 @@ function classical_Potts(q::Int64, β::Float64)
     end
     return A_potts
 end
+
+function classical_Clock(q::Int64, β::Float64)
+    V = ℂ^q
+    A_clock = TensorMap(zeros, V ⊗ V ← V ⊗ V)
+    clock(i, j) = -cos(2π / q * (i - j))
+
+    for i in 1:q
+        for j in 1:q
+            for k in 1:q
+                for l in 1:q
+                    E = clock(i, j) + clock(j, k) + clock(k, l) + clock(l, i)
+                    A_clock[i, j, k, l] = exp(-β * E)
+                end
+            end
+        end
+    end
+    return A_clock
+end
