10c17677321b8b62905266b59893ca1166d04630
victor <victor.vanthilt@gmail.com>
2025-02-08 21:48:41 +0100

    format

--- src/models/ising.jl
+++ src/models/ising.jl
@@ -9,8 +9,8 @@ function classical_ising(β::Number; h=0)
     end
 
     T_array = Float64[exp(β * (σ(i)σ(j) + σ(j)σ(k) + σ(k)σ(l) + σ(l)σ(i)) +
-                   h / 2 * β * (σ(i) + σ(j) + σ(k) + σ(l)))
-               for i in 1:2, j in 1:2, k in 1:2, l in 1:2]
+                          h / 2 * β * (σ(i) + σ(j) + σ(k) + σ(l)))
+                      for i in 1:2, j in 1:2, k in 1:2, l in 1:2]
 
     T = TensorMap(T_array, ℝ^2 ⊗ ℝ^2 ← ℝ^2 ⊗ ℝ^2)
 
