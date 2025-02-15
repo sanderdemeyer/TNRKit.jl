116d80ad5c5868ecc61ef07398bbf4d18ad18e8a
Victor Vanthilt <73738005+VictorVanthilt@users.noreply.github.com>
2025-02-15 11:56:16 +0100

    Small permute and contraction order changes in `TRG` and `BTRG` (#22)
    * get rid of unnecessary permutes in `TRG`
    
    * implement the optimal contraction ordering for `BTRG`

--- src/schemes/trg.jl
+++ src/schemes/trg.jl
@@ -17,17 +17,12 @@ function step!(scheme::TRG, trunc::TensorKit.TruncationScheme)
 
     U, S, V, _ = tsvd(scheme.T, ((1, 4), (2, 3)); trunc=trunc)
 
-    # Flip legs to their original domain (to mitigate space mismatch at the end)
-    U = permute(U, ((1,), (2, 3)))
-    V = permute(V, ((1, 2), (3,)))
-
     @plansor begin
-        C[-1; -2 -3] := U[-1; -2 1] * sqrt(S)[1; -3]
-        D[-1 -2; -3] := sqrt(S)[-1; 1] * V[1 -2; -3]
+        C[-1 -2; -3] := U[-1 -2; 1] * sqrt(S)[1; -3]
+        D[-1; -2 -3] := sqrt(S)[-1; 1] * V[1; -2 -3]
     end
 
-    # @plansor complains here, not sure why
-    @tensor scheme.T[-1 -2; -3 -4] := D[-1 1; 4] * B[-2; 3 1] * C[3; 2 -3] * A[4 2; -4]
+    @tensor scheme.T[-1 -2; -3 -4] := D[-1; 1 4] * B[-2; 3 1] * C[3 2; -3] * A[4 2; -4]
     return scheme
 end
 
