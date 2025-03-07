39596e7518c01968a815a024c0777926a7b84ee1
Adwait-Naravane <adwaitnaravane@gmail.com>
2025-03-07 16:38:00 +0100

    Modifying Loop TNR

--- src/TNRKit.jl
+++ src/TNRKit.jl
@@ -17,6 +17,7 @@ include("schemes/gilt.jl")
 include("schemes/gilt-tnr.jl")
 include("schemes/atrg.jl")
 include("schemes/slooptnr.jl")
+include("schemes/looptnr.jl")
 
 export TNRScheme
 export TRG, trg_convcrit
@@ -25,7 +26,7 @@ export HOTRG, hotrg_convcrit
 export ATRG, atrg_convcrit
 export GILTTNR, gilttnr_convcrit
 export SLoopTNR
-
+export LoopTNR, Ψ_A, Ψ_B, entanglement_filtering!, loop_opt!
 export run!
 
 # models
