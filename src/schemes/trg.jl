647433d90afd15a8c2db7696d6d55f41fb3868d1
Chenqi Meng <mcqgravity@gmail.com>
2025-08-08 15:59:11 +0800

    A trivial simplification of TRG (#77)
    * A simplification of TRG
    
    * Some format

--- src/schemes/trg.jl
+++ src/schemes/trg.jl
@@ -35,21 +35,10 @@ mutable struct TRG <: TNRScheme
 end
 
 function step!(scheme::TRG, trunc::TensorKit.TruncationScheme)
-    U, S, V, _ = tsvd(scheme.T, ((1, 2), (3, 4)); trunc = trunc)
-
-    @tensor begin
-        A[-1 -2; -3] := U[-1 -2; 1] * sqrt(S)[1; -3]
-        B[-1; -2 -3] := sqrt(S)[-1; 1] * V[1; -2 -3]
-    end
-
-    U, S, V, _ = tsvd(scheme.T, ((3, 1), (4, 2)); trunc = trunc)
-
-    @tensor begin
-        C[-1 -2; -3] := U[-1 -2; 1] * sqrt(S)[1; -3]
-        D[-1; -2 -3] := sqrt(S)[-1; 1] * V[1; -2 -3]
-    end
-
-    @tensor scheme.T[-1 -2; -3 -4] := D[-1; 3 1] * B[-2; 1 4] * C[2 4; -4] * A[3 2; -3]
+    A, B = SVD12(scheme.T, trunc)
+    Tp = transpose(scheme.T, ((2, 4), (1, 3)))
+    C, D = SVD12(Tp, trunc)
+    @planar scheme.T[-1 -2; -3 -4] := D[-2; 1 2] * B[-1; 4 1] * C[4 3; -3] * A[3 2; -4]
     return scheme
 end
 
