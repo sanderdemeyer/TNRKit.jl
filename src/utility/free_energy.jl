567e9028457ccfc7afe11ba28af30f5dfaaa676a
victor <victor.vanthilt@gmail.com>
2025-08-18 10:08:50 +0200

    remove redundant sentence

--- src/utility/free_energy.jl
+++ src/utility/free_energy.jl
@@ -4,8 +4,6 @@ $(SIGNATURES)
 Takes a vector of normalization factors `data` and a given inverse temperature `β`
 and computes the free energy.
 
-The `initial_size` is used to set the initial size of the physical lattice, which is typically `1.0` (or `2.0` for the non-symmetric classical Ising model).
-
 !!! info
     The `scalefactor` should be set to the rescaling factor of the area of the tensor network after each iteration of the TNR algorithm.
 
