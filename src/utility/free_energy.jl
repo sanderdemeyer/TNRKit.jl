11522951a317dd6640294a89a74515fb329da3a7
victor <victor.vanthilt@gmail.com>
2025-08-18 09:55:29 +0200

    update `free_energy` kwarg name and add extra ising docstrings

--- src/utility/free_energy.jl
+++ src/utility/free_energy.jl
@@ -1,4 +1,7 @@
-function free_energy(data, β; scalefactor = 2.0, unitcell = 1.0)
+"""
+
+"""
+function free_energy(data, β; scalefactor = 2.0, initial_size = 2.0)
     lnz = 0.0
     x = 1.0 - log(unitcell) / log(scalefactor)
     for (i, z) in enumerate(data)
