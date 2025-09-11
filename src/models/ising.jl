0687d541193e0938658d831e5101134ed238f0f2
Victor Vanthilt <73738005+VictorVanthilt@users.noreply.github.com>
2025-09-11 09:26:51 +0200

    Change `classical_ising` to use `ComplexSpace` instead of `CartesianSpace` (#93)

--- src/models/ising.jl
+++ src/models/ising.jl
@@ -36,7 +36,7 @@ function classical_ising(β::Number; h = 0)
             for i in 1:2, j in 1:2, k in 1:2, l in 1:2
     ]
 
-    T = TensorMap(T_array, ℝ^2 ⊗ ℝ^2 ← ℝ^2 ⊗ ℝ^2)
+    T = TensorMap(T_array, ℂ^2 ⊗ ℂ^2 ← ℂ^2 ⊗ ℂ^2)
 
     return T
 end
