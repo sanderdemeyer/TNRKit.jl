31a6a91b94bbaf16360763eeefd4594cdad60fa3
Yue Zhengyuan <yuezy1997@icloud.com>
2025-10-14 16:17:23 +0800

    Fix CFT spectrum per sector calculation (#106)
    * Fix CFT spectrum per sector calculation
    
    * Display scaling dimension in loop-TNR test
    
    * Relax rtol in Ising loop-TNR CFT test

--- src/utility/cft.jl
+++ src/utility/cft.jl
@@ -162,45 +162,40 @@ function spec(TA::TensorMap, TB::TensorMap, shape::Array; Nh = 25)
         throw(ArgumentError("Sectors with non-Bosonic charge $I has not been implemented"))
     end
 
-    spec_sector = Dict()
-    conformal_data = Dict()
-
-    for charge in values(I)
-        if I == Trivial
-            V = 𝔽^1
-        else
-            V = Vect[I](charge => 1)
-        end
-
-        if shape ≈ [1, 4, 1]
-            x = rand(domain(TA)[1] ⊗ domain(TB)[1] ⊗ domain(TA)[1] ⊗ domain(TB)[1] ← V)
-            f = MPO_action_1x4_twist
-        elseif shape ≈ [1, 8, 1]
-            x = rand(domain(TA)[1] ⊗ domain(TB)[1] ⊗ domain(TA)[1] ⊗ domain(TB)[1] ← V)
-            f = MPO_action_1x4
-        elseif shape ≈ [sqrt(2), 2 * sqrt(2), 0] ||
-                shape ≈ [4 / sqrt(10), 2 * sqrt(10), 2 / sqrt(10)]
-            x = rand(domain(TB) ⊗ domain(TB) ← V)
-            f = MPO_action_2gates
-        end
-
-        if dim(x) == 0
-            spec_sector[charge] = [0.0]
-        else
-            spec, _, _ = eigsolve(
-                a -> f(TA, TB, a), x, Nh, :LM; krylovdim = 40, maxiter = 100,
-                tol = 1.0e-12,
-                verbosity = 0
-            )
-
-            spec_sector[charge] = filter(x -> abs(real(x)) ≥ 1.0e-12, spec)
-        end
+    xspace, f = if shape ≈ [1, 4, 1]
+        domain(TA)[1] ⊗ domain(TB)[1] ⊗ domain(TA)[1] ⊗ domain(TB)[1],
+            MPO_action_1x4_twist
+    elseif shape ≈ [1, 8, 1]
+        domain(TA)[1] ⊗ domain(TB)[1] ⊗ domain(TA)[1] ⊗ domain(TB)[1],
+            MPO_action_1x4
+    elseif shape ≈ [sqrt(2), 2 * sqrt(2), 0] ||
+            shape ≈ [4 / sqrt(10), 2 * sqrt(10), 2 / sqrt(10)]
+        domain(TB) ⊗ domain(TB), MPO_action_2gates
     end
+
+    spec_sector = Dict(
+        map(sectors(fuse(xspace))) do charge
+            V = (I == Trivial) ? 𝔽^1 : Vect[I](charge => 1)
+            x = rand(xspace ← V)
+            if dim(x) == 0
+                return charge => [0.0]
+            else
+                spec, _, _ = eigsolve(
+                    a -> f(TA, TB, a), x, Nh, :LM; krylovdim = 40, maxiter = 100,
+                    tol = 1.0e-12,
+                    verbosity = 0
+                )
+                return charge => filter(x -> abs(real(x)) ≥ 1.0e-12, spec)
+            end
+        end
+    )
+
+    conformal_data = Dict()
 
     norm_const_0 = spec_sector[one(I)][1]
     conformal_data["c"] = 6 / pi / (Reτ - area / 4) * log(norm_const_0)
 
-    for charge in values(I)
+    for charge in sectors(fuse(xspace))
         DeltaS = -1 / (2 * pi * shape[1] / shape[2]) *
             log.(spec_sector[charge] / norm_const_0)
         if !(relative_shift ≈ 0)
--- test/schemes.jl
+++ test/schemes.jl
@@ -108,14 +108,18 @@ end
 
     for shape in [[1, 4, 1], [sqrt(2), 2 * sqrt(2), 0]]
         cft = cft_data!(scheme, shape)
-        @test real(cft[Z2Irrep(1)][1]) ≈ ising_cft_exact[1] rtol = 2.0e-4
-        @test real(cft[Z2Irrep(0)][2]) ≈ ising_cft_exact[2] rtol = 1.0e-2
+        d1, d2 = real(cft[Z2Irrep(1)][1]), real(cft[Z2Irrep(0)][2])
+        @info "Obtained lowest scaling dimensions:\n$(d1), $(d2)."
+        @test d1 ≈ ising_cft_exact[1] rtol = 5.0e-4
+        @test d2 ≈ ising_cft_exact[2] rtol = 1.0e-2
     end
 
     for shape in [[1, 8, 1], [4 / sqrt(10), 2 * sqrt(10), 2 / sqrt(10)]]
         cft = cft_data!(scheme, shape, truncdim(8), truncbelow(1.0e-10))
-        @test real(cft[Z2Irrep(1)][1]) ≈ ising_cft_exact[1] rtol = 1.0e-2
-        @test real(cft[Z2Irrep(0)][2]) ≈ ising_cft_exact[2] rtol = 1.0e-2
+        d1, d2 = real(cft[Z2Irrep(1)][1]), real(cft[Z2Irrep(0)][2])
+        @info "Obtained lowest scaling dimensions:\n$(d1), $(d2)."
+        @test d1 ≈ ising_cft_exact[1] rtol = 1.0e-2
+        @test d2 ≈ ising_cft_exact[2] rtol = 1.0e-2
     end
 end
 
